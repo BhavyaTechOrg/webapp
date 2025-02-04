a3f68a526af928e0074cd5f40573f862ed57c01b
reddy-bhavya <raghunathareddy.b@northeastern.edu>
2025-02-04 14:58:06 -0500

    Modified script.sh

--- packer/script.sh
+++ packer/script.sh
@@ -16,16 +16,16 @@ echo "Creating database 'csye6225db'..."
 sudo -u postgres psql -c "CREATE DATABASE csye6225db;"
 
 # Create a Linux group and user
-echo "Creating Bhavya_RaghunathaReddy_002415278_01 group and user..."
+echo "Creating webapp-main group and user..."
 sudo groupadd csye6225group
 sudo useradd -m -g csye6225group -s /bin/bash csye6225user
 
-# Unzip the Bhavya_RaghunathaReddy_002415278_01 to /opt/csye6225
-echo "Unzipping Bhavya_RaghunathaReddy_002415278_01..."
+# Unzip the webapp-main to /opt/csye6225
+echo "Unzipping webapp-main..."
 sudo mkdir -p /opt/csye6225
-sudo mv /path/to/Bhavya_RaghunathaReddy_002415278_01.zip /opt/csye6225/
+sudo mv /path/to/webapp-main.zip /opt/csye6225/
 sudo apt-get install unzip -y
-sudo unzip /opt/csye6225/Bhavya_RaghunathaReddy_002415278_01.zip -d /opt/csye6225
+sudo unzip /opt/csye6225/webapp-main.zip -d /opt/csye6225
 
 # Update permissions
 echo "Updating permissions..."
